7f62b9768a72c66c1642f85a4176ee20fe4f97f7
Uriel Solis <euss99@hotmail.com>
2023-07-30 01:28:36 -0600

    Init the project

---
+++ src/components/Formulario.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="md:w-1/2">
+    <h2 class="font-black text-3xl text-center">Seguimiento de pacientes</h2>
+
+    <p class="text-lg mt-5 text-center mb-10">
+      Añade pacientes y
+      <span class="text-indigo-600 font-bold">adminístralos</span>
+    </p>
+  </div>
+</template>
